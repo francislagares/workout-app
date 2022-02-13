36d03f5a4b6f7604ca0142201133f38809cc5158
Francis Lagares <francis.lagares@gmail.com>
2022-02-13 18:14:55 +0100

    feat: render HomeScreen data

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
+import WorkoutItem from '../components/Workout/WorkoutItem';
+import data from '../data.json';
+import { IWorkout } from 'types/data';
 
 const HomeScreen = ({ navigation }: NativeStackHeaderProps) => {
   useEffect(() => {
@@ -10,8 +13,13 @@ const HomeScreen = ({ navigation }: NativeStackHeaderProps) => {
   });
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>I am home screen</Text>
+      <FlatList
+        data={data as IWorkout[]}
+        keyExtractor={item => item.slug}
+        renderItem={WorkoutItem}
+      />
       <Button
         title='Go to Planner'
         onPress={() => navigation.push('Planner')}
@@ -21,3 +29,15 @@ const HomeScreen = ({ navigation }: NativeStackHeaderProps) => {
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+  },
+  header: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+});
